67f66fd0f88c9c760cd34365a853ac1fc25bee14
anibal <anibaltoro.a@gmail.com>
2016-10-16 01:48:20 -0300

    Incorporacion de Procedimiento almacenado en MySql

---
+++ BD/Procediminetos Almacenados/Menu/listarMenu.sql
@@ -0,0 +1,25 @@
+USE `portal`;
+DROP procedure IF EXISTS `listarMenu`;
+
+DELIMITER $$
+USE `portal`$$
+CREATE PROCEDURE `listarMenu` (in idMenu int, in nom varchar(50),in nvl int)
+BEGIN
+
+	select 
+	m.id_menu,
+	m.nombre,
+	m.id_url,
+	m.nivel,
+	u.url
+	from menu m left join urls u 
+	on (m.id_url = u.id_url)
+	where
+	(idMenu is null or idMenu = m.id_menu) and
+	(nom is null or nom = m.nombre) and
+	(nvl is null or nvl = m.nivel)
+	order by m.orden;	
+
+END$$
+
+DELIMITER ;
