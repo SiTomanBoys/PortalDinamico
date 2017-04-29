325bf87dd33a171694b471667197113cf693b3e0
anibal <anibaltoro.a@gmail.com>
2017-04-29 18:53:01 -0300

    Insert por defecto

---
+++ BD/Datos Por Defecto/idiomas.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 29, 2017 at 06:50 PM
+-- Server version: 5.5.54-0ubuntu0.12.04.1
+-- PHP Version: 5.3.10-1ubuntu3.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zadmin_portal`
+--
+
+--
+-- Dumping data for table `idiomas`
+--
+
+INSERT INTO `idiomas` (`id_idioma`, `descripcion`) VALUES
+(1, 'Default'),
+(2, 'Ingles'),
+(3, 'Español');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
