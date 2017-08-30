ab7652c20df6a9e921691056ae5839a7819b2ba3
Anibal Toro <anibal.toro@paygroup.com>
2017-08-30 19:46:19 -0300

    se agrega servlet y xsl del pie de pagina.

---
+++ javaEnterprise/Portal/src/cl/portaldinamico/Servlets/Pie.java
@@ -0,0 +1,68 @@
+package cl.portaldinamico.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import cl.portaldinamico.constants.Constants;
+import cl.portaldinamico.mybatis.ConsultaMyBatis;
+import cl.portaldinamico.utils.Ejb3Utils;
+import cl.portaldinamico.utils.Ejb3UtilsLocal;
+
+/**
+ * Servlet implementation class pie
+ */
+public class Pie extends Base {
+	private static final long serialVersionUID = 1L;
+	static final Logger log = Logger.getLogger(Pie.class);
+    public Pie() {
+        super();
+    }
+
+    protected void procesarPeticion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+    	Ejb3UtilsLocal utils = new Ejb3Utils();
+		HttpSession session= request.getSession(true);
+		HashMap<String,Object> datosConf = new HashMap<String,Object>();
+		if(session.getAttribute("datosConf")!= null)
+			datosConf = (HashMap<String,Object>) session.getAttribute("datosConf");
+		try
+		{
+			String catalogo = datosConf.get(Constants.catalogoBase).toString();
+			String servidores = datosConf.get(Constants.servidoresBase).toString();
+			ConsultaMyBatis ex = new ConsultaMyBatis(servidores,catalogo);
+			HashMap<String,Object> p = new HashMap<String,Object>();
+			p.put("nombre", "pie");
+			p.put("id_idioma", 1);
+			//Obtengo el XSL
+			String XSL = ex.SelectValor(datosConf.get(Constants.jndiBase).toString(), "coreXSLPrincipalMapper.xml", "coreXSLPrincipal.getXSL", p, "contenido");
+			String XML = getXMLPie(datosConf);
+			PrintWriter out = response.getWriter();
+            String html = utils.generarDocumento(XML, XSL);
+            out.println(html);
+		}catch(Exception ex)
+		{
+			utils.impLog(log,Level.ERROR_INT, datosConf, "ERROR AL GENERAR PIE DE PAGINA", ex);
+			response.sendRedirect("/Portal/error?Id=18");
+		}
+	}
+
+    private String getXMLPie(HashMap<String,Object> datosConf)
+	{
+		String XML ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		XML+="<Documento>";
+		XML+="<Titulo>"+datosConf.get(Constants.TituloPortal)+"</Titulo>";
+		XML+="</Documento>";
+		return XML;
+	}
+
+}
---
+++ xsls/xsls_principales/pie.xsl
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Toda hoja de transformacion comineza con este tag -->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<!-- Indicamos que nuestro output sera un tipo HTML -->
+	<xsl:output method = "html" />
+	<!-- Usamos Xpath para comentar que queremos parsear todo el xml -->
+	<xsl:template match="/Documento">
+		<html>
+			<head>
+			<title><xsl:value-of select="Titulo" /></title>
+            	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+				<meta http-equiv='cache-control' content='no-cache' />
+				<meta http-equiv='pragma' content='no-cache' />
+				<meta http-equiv='expires' content='1' />
+				<link type="text/css" href="/css/estilos.css" rel="stylesheet" />
+				<style>
+					.footer {
+						background-color: #C1C1C1;
+						color: #000000;
+						border-top-style: solid;
+						border-top-width: 1px;
+						border-top-color: #999999;
+					}
+				</style>
+			</head>
+			
+            <body>
+				<table cellpadding="0" cellspacing="0" border="0" width="100%" class="footer" height="32">
+					<tr>
+						<td align="center">d-portal | &copy;2017. Todos los derechos reservados</td>
+					</tr>
+				</table>
+			</body>
+		</html>
+	</xsl:template>
+</xsl:stylesheet>
